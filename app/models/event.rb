5f92103106ea913e90bb182c0ec6176e25051332
MikeAmato <mike.blackmist@aemail4u.com>
2014-11-20 09:12:22 -0500

    added to gemfile for heroku push

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
   belongs_to :free_time
+
+
+
+
+  @response = HTTParty.get("http://api.eventful.com/json/events/search?&location=Atlanta&app_key=b8hsZDFcNgvxWm4j")
 end
